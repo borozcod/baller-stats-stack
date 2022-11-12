4045a08d25cb6e2ab053e9cbcf66f9ae119cb604
Bryan Orozco <bryanorozcoweb@gmail.com>
2022-11-11 22:40:52 -0800

    dev: cdk init

---
+++ test/baller-stats-stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BallerStatsStack from '../lib/baller-stats-stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/baller-stats-stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BallerStatsStack.BallerStatsStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
